f19185dc237d0444dca2461c98f01e424f504190
Ankit <jain2anki@gmail.com>
2019-10-03 23:25:47 +0530

    leaderboard

--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
@@ -8,32 +9,118 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  var snap = Firestore.instance
+      .collection("questions")
+      .document("Ixcm9NHAPQbkQRbLK0qa");
+  bool check = true;
+  var level = 1, question = "qq", answer;
+
+  // getlevel() {
+  //   // Firestore.instance
+  //   // .collection("questions")
+  //   // .where("level",isEqualTo :"${snapshot.data.data["level"]}").
+  //   // Firestore.instance.collection("teams").where("teamId",isEqualTo:"BTlSYoCGqtmVgwIpHJmM").snapshots();
+  //   Firestore.instance
+  //       .collection("teams")
+  //       .document("BTlSYoCGqtmVgwIpHJmM")
+  //       .get()
+  //       .then((doc) {
+  //     print(doc["level"]);
+  //     setState(() {
+  //       level = doc["level"];
+  //     });
+  //     getquestion();
+  //   });
+  // }
+
+  // getquestion() {
+  //   snap.get().then((data) {
+  //     print(data["question"]);
+  //     print(data["answer"]);
+  //     setState(() {
+  //       question = data["question"];
+  //       answer = data["answer"];
+  //       check = true;
+  //     });
+  //   });
+  // }
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-           Text("Treasure 1",style: TextStyle(fontSize: 40),),
-           Padding(
-             padding: const EdgeInsets.all(20.0),
-             child: Text("Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",textAlign: TextAlign.center,style: TextStyle(),),
-           ),
-          RaisedButton(
-            onPressed: () {
-              _scanQR();
-            },
-            child: Text("Scan"),
-          ),
-        ],
-      ),
-    ));
+  void initState() {
+    // getlevel();
+    super.initState();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return !check
+        ? CircularProgressIndicator()
+        : Container(
+            child: Center(
+            child: StreamBuilder(
+                stream: Firestore.instance
+                    .collection("teams")
+                    .document("BTlSYoCGqtmVgwIpHJmM")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
 
-    Future _scanQR() async {
+                  print("${snapshot.data.data["level"]};");
+                  if (snapshot.data.data["level"] > 10) {
+                    return Container(
+                      child: Center(
+                        child: Text("Win"),
+                      ),
+                    );
+                  }
+                  getq(l) async {
+                    QuerySnapshot data = await Firestore.instance
+                        .collection("questions")
+                        .where("level", isEqualTo: "$l")
+                        .getDocuments();
+                    var list = data.documents;
+                    print(list[0].documentID);
+                    // setState(() {
+                    //   level = l;
+                    //   // question = list[0]["question"];
+                    // });
+                    return true;
+                  }
+                
+                  getq("${snapshot.data.data["level"]}");
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Treasure ${snapshot.data.data["level"]}",
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text("$question"),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          " Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(),
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          _scanQR();
+                        },
+                        child: Text("Scan"),
+                      ),
+                    ],
+                  );
+                }),
+          ));
+  }
+
+  Future _scanQR() async {
     try {
       String qrResult = await BarcodeScanner.scan();
       setState(() {
@@ -42,7 +129,6 @@ class _GameState extends State<Game> {
         // result = qrResult;
       });
     } on PlatformException catch (ex) {
-
       if (ex.code == BarcodeScanner.CameraAccessDenied) {
         setState(() {
           // result = "Camera permission was denied";
--- lib/lb.dart
+++ lib/lb.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class Lb extends StatefulWidget {
   @override
   _LbState createState() => _LbState();
@@ -8,26 +10,36 @@ class _LbState extends State<Lb> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context,index){
-          return Card(
-            child: Row(
-              children: <Widget>[
-                CircleAvatar(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Team Under pressure"),
-                ),
-                Spacer(),
-                Text("${index+1}"),
-              ],
-            ),
-          );
-        },
-
-      ),
-    );
+        padding: const EdgeInsets.all(25.0),
+        child: StreamBuilder(
+          stream: Firestore.instance
+              .collection("teams")
+              .orderBy("level", descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || snapshot.hasError) {
+              return CircularProgressIndicator();
+            }
+            print("${snapshot.data.data["teamId"]}");
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      CircleAvatar(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("${snapshot.data}"),
+                      ),
+                      Spacer(),
+                      // Text("${index+1}"),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
