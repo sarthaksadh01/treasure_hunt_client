b56b8826944fe64ad5a1f77ba3e69e889c12b98d
Sarthak sadh <sarthaksadh01@gmail.com>
2019-10-04 01:07:26 +0530

    changed primary color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-        primaryColor: Colors.blue, accentColor: Colors.blueAccent),
+        primaryColor: Colors.teal, accentColor: Colors.tealAccent),
     title: 'Treasure Hunt',
     initialRoute: '/',
     routes: {
